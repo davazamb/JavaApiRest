d4f32fa8bcc91f36edb8e2bf0af53d95f9defb17
davazamb <davazamb10@outlook.com>
2025-02-17 10:33:47 -0300

    First Commit

---
+++ JavaApiRest/src/main/java/DZ/JavaApiRest/JavaApiRestApplication.java
@@ -0,0 +1,13 @@
+package DZ.JavaApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiRestApplication.class, args);
+	}
+
+}
---
+++ JavaApiRest/src/test/java/DZ/JavaApiRest/JavaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package DZ.JavaApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
